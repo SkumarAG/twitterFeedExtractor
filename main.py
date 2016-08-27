c5872d1b4a42d774875754e69a39cefc1e2bb325
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2016-08-27 18:53:25 -0400

    house Keeping

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#from twitter_feed import twitterData,tweetData,saveTweets#not need
-#from mysql.connector import connect, Error# not need
 from files.db_query import saveTweets
 
 twitter_id = "@trafficjamjen"
